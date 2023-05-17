1e8ac496240905042ceb1d09c9183ae034f1437a
Sangrim.Choi <ds1hvh@gmail.com>
2023-05-17 12:22:09 +0900

    obs-0004 added

--- live-chart.py
+++ live-chart.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def obs():
-    return render_template('index.html', data='test')
+    #return render_template('index.html', data='test')
+    return render_template('index2.html', data='test')
 
 @app.route('/obs02')
 def obs2():
@@ -19,6 +20,10 @@ def obs2():
 @app.route('/obs03')
 def obs3():
     return render_template('index3.html', data='test')
+
+@app.route('/obs04')
+def obs4():
+    return render_template('index4.html', data='test')
 
 @app.route('/obs-0001')
 def live_data():
@@ -90,6 +95,29 @@ def live_data3():
     response.content_type = 'application/json'
     return response
 
+@app.route('/obs-0004')
+def live_data4():
+    res = requests.get('http://125.141.107.23:8080/api/getObsData?obsID=OBS-0004')
+    json_Object = json.loads(res.text)
+    print(f'Get Request Response : {res.text}')
+
+    jsonArray = json_Object.get("obs_data")
+    a = int(jsonArray[0]['DATETIME'])
+    b = float(jsonArray[0]['DATA'])
+    print(f'Get obs-0002 Request Response : {a} : {b}')
+
+    data = [((int(jsonArray[0]['DATETIME']) + 0) * 1000 , float(jsonArray[0]['DATA'])),
+            ((int(jsonArray[1]['DATETIME']) + 0) * 1000 , float(jsonArray[1]['DATA'])),
+            ((int(jsonArray[2]['DATETIME']) + 0) * 1000 , float(jsonArray[2]['DATA'])),
+            ((int(jsonArray[3]['DATETIME']) + 0) * 1000 , float(jsonArray[3]['DATA'])),
+            ((int(jsonArray[4]['DATETIME']) + 0) * 1000 , float(jsonArray[4]['DATA'])),
+            ((int(jsonArray[5]['DATETIME']) + 0) * 1000 , float(jsonArray[5]['DATA'])),
+            ((int(jsonArray[6]['DATETIME']) + 0) * 1000 , float(jsonArray[6]['DATA'])),
+            ((int(jsonArray[7]['DATETIME']) + 0) * 1000 , float(jsonArray[7]['DATA'])) ]
+
+    response = make_response(json.dumps(data))
+    response.content_type = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
---
+++ static/js/obs04_charts.js
@@ -0,0 +1,83 @@
+var chart;
+
+/**
+ * Request data from the server, add it to the graph and set a timeout
+ * to request again
+ */
+function requestOBS02_Data() {
+    $.ajax({
+        url: '/obs-0004',
+        success: function(point) {
+            var series = chart.series[0],
+                shift = series.data.length > 40; // shift if the series is
+                                                 // longer than 40
+
+            // add the point
+            chart.series[0].addPoint(point[0], true, shift);
+            chart.series[1].addPoint(point[1], true, shift);
+            chart.series[2].addPoint(point[2], true, shift);
+            chart.series[3].addPoint(point[3], true, shift);
+            chart.series[4].addPoint(point[4], true, shift);
+            chart.series[5].addPoint(point[5], true, shift);
+            chart.series[6].addPoint(point[6], true, shift);
+            chart.series[7].addPoint(point[7], true, shift);
+
+            // call it again after one second
+            setTimeout(requestOBS02_Data, 1000);
+        },
+        cache: false
+    });
+}
+
+$(document).ready(function() {
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'obs-0004-data-container',
+            defaultSeriesType: 'spline',
+            events: {
+                load: requestOBS02_Data
+            }
+        },
+        title: {
+            text: 'Realtime Sensor data'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 100,
+            maxZoom: 20 * 1000
+        },
+        yAxis: {
+            minPadding: 0.001,
+            maxPadding: 0.001,
+            title: {
+                text: 'Value [kPa]',
+                margin: 10
+            }
+        },
+        series: [{
+            name: 'CH1',
+            data: []
+        },{
+            name: 'CH2',
+            data: []
+        },{
+            name: 'CH3',
+            data: []
+        },{
+            name: 'CH4',
+            data: []
+        },{
+            name: 'CH5',
+            data: []
+        },{
+            name: 'CH6',
+            data: []
+        },{
+            name: 'CH7',
+            data: []
+        },{
+            name: 'CH8',
+            data: []
+        }]
+    });
+});
